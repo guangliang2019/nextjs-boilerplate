11922af354ed6d3f4bcfa6080320403e6cd8c137
yangguangliang <yangguangliang@hetao101.com>
2024-08-06 21:17:32 +0800

    feat: 随便做些更改

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
@@ -40,25 +41,26 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        {/* <a
+          href="https://baidu.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        > */}
           <h2 className="mb-3 text-2xl font-semibold">
             文档{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          <div className="h-4 w-4 rounded bg-teal-500" onClick={() => window.alert('hello')} ></div>
+          <p className="m-0 text-white-500  max-w-[30ch] text-sm opacity-50">
             Find in-depth information about Next.js features and API.
           </p>
-        </a>
+        {/* </a> */}
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://baidu.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
